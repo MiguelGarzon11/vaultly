7962a32d7ff5bd2ddec5fd70c6561110733ba9a9
Miguel Angel <miguelangelgarb@gmail.com>
2025-10-15 21:09:02 -0500

    Update Astro configuration and package dependencies for server output and Tailwind CSS support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+import node from '@astrojs/node';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
