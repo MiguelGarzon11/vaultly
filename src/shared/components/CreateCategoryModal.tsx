642ad1f8abb7dd521baebdc85f100965a7bfe758
Miguel Angel <miguelangelgarb@gmail.com>
2025-12-15 10:57:57 -0500

    Modal new category terminada

--- src/shared/components/CreateCategoryModal.tsx
+++ src/shared/components/CreateCategoryModal.tsx
@@ -20,7 +20,7 @@ export default function CreateCategoryModal({isOpen, onOpenChange
   const colors = [
     { name: "red", code: "#ED4626"},
     { name: "orange", code: "#ED8E1A"},
-    { name: "yellow", code: "#F4D50B"},
+    { name: "yellow", code: "#C8C109"},
     { name: "green", code: "#10b981"},
     { name: "cyan", code: "#0BC5F4"},
     { name: "blue", code: "#3b82f6"},
@@ -30,6 +30,23 @@ export default function CreateCategoryModal({isOpen, onOpenChange
 
   const icons = [
     {name: "compras", ref: "/assets/iconsC/compras.svg"},
+    {name: "hogar", ref: "/assets/iconsC/hogar.svg"},
+    {name: "transporte", ref: "/assets/iconsC/transporte.svg"},
+    {name: "comida", ref: "/assets/iconsC/comida.svg"},
+    {name: "entretenimiento", ref: "/assets/iconsC/entretenimiento.svg"},
+    {name: "salud", ref: "/assets/iconsC/salud.svg"},
+    {name: "educacion", ref: "/assets/iconsC/educacion.svg"},
+    {name: "viaje", ref: "/assets/iconsC/viaje.svg"},
+    {name: "ropa", ref: "/assets/iconsC/ropa.svg"},
+    {name: "tecnologia", ref: "/assets/iconsC/tecnologia.svg"},
+    {name: "regalo", ref: "/assets/iconsC/regalo.svg"},
+    {name: "cafe", ref: "/assets/iconsC/cafe.svg"},
+    {name: "entrenamiento", ref: "/assets/iconsC/entrenamiento.svg"},
+    {name: "musica", ref: "/assets/iconsC/musica.svg"},
+    {name: "pelicula", ref: "/assets/iconsC/pelicula.svg"},
+    {name: "otros", ref: "/assets/iconsC/otros.svg"},
+
+
   ]
   return createPortal (
     <>
@@ -38,15 +55,15 @@ export default function CreateCategoryModal({isOpen, onOpenChange
 
         {/* Modal */}
         <div className="fixed inset-0 z-50 flex items-center justify-center">
-          <div className="w-[90%] sm:w-[70%] md:w-[60%] lg:max-w-[600px] bg-white h-[500px] transition-all duration-200 rounded-t-xl rounded-b-3xl overflow-hidden drop-shadow-xl flex flex-col justify-start items-center" onClick={(e) => e.stopPropagation()}>
-              <div className="h-[45%] w-full px-8" style={{backgroundColor: color }}>
+          <div className="w-[90%] sm:w-[70%] md:w-[60%] lg:max-w-[600px] bg-white h-auto sm:h-[650px] transition-all duration-200 rounded-t-xl rounded-b-3xl overflow-hidden drop-shadow-xl flex flex-col justify-start items-center" onClick={(e) => e.stopPropagation()}>
+              <div className="h-[30%] w-full px-8" style={{backgroundColor: color }}>
                   {/* Vista previa */}
                   <section className="flex flex-col size-full gap-5 justify-center items-start">
                     <h1 className="text-white text-lg font-medium">Nueva Categoría</h1>
                     <div className="flex w-full space-x-3 items-center">
                       <div className="size-12 rounded-xl flex justify-center items-center" style={{backgroundColor: `color-mix(in srgb, ${color} 80%, white)` }}>
                         <svg className="size-8 text-white">
-                          <use href="/assets/iconsC/compras.svg"></use>
+                          <use href={icon}></use>
                         </svg>
                       </div>
                       <div className="flex flex-col justify-start items-start">
@@ -65,9 +82,9 @@ export default function CreateCategoryModal({isOpen, onOpenChange
               </div>
               {/* Formulario */}
               <section className="size-full">
-                <form className="size-full px-16 py-6 flex flex-col space-y-4">
+                <form className="size-full px-12 py-8 flex flex-col space-y-4">
                   {/* Nombre de la categoría */}
-                    <div className="flex flex-col space-y-2 w-">
+                    <div className="flex flex-col space-y-2">
                       <label htmlFor="title" className="text-text text-sm font-medium">Nombre de la categoría</label>
                       <input type="text" name="title" id="titleCategory" className="h-8 w-full bg-dark-back rounded-lg px-2 text-sm text-text font-light" placeholder="Ej: Supermercado, Entretenimiento, Gym..."/>
                     </div>
@@ -78,7 +95,7 @@ export default function CreateCategoryModal({isOpen, onOpenChange
                       <p className="absolute top-9 left-2 text-xs">COP</p>
                     </div>
                   {/* Color */}
-                    <div className="flex flex-col size-full space-y-2">
+                    <div className="flex flex-col w-full space-y-2">
                       <p className="text-text text-sm font-medium">Color</p>
                       <div className="flex flex-wrap size-full space-x-4 justify-center">
                           {colors.map((c) => (
@@ -98,13 +115,21 @@ export default function CreateCategoryModal({isOpen, onOpenChange
                       </div>
                     </div>
                   {/* Icono */}
-                    <div className="flex flex-col size-full space-y-2">
+                    <div className="flex flex-col gap-2 size-full">
                       <p className="text-text text-sm font-medium">Icono</p>
-                      <div className="flex flex-wrap size-full space-x-4 justify-center">
+                      <div className="flex flex-wrap size-full gap-2 justify-center items-center">
                           {icons.map((i) => (
-                            <button type="button">
-                              <div className="flex p-2 size-10 rounded-lg items-center justify-center" style={{backgroundColor: `color-mix(in srgb, ${color} 10%, white)`}}> 
-                                <svg className="size-7" style={{color: `color-mix(in srgb, ${color} 80%, white)`}}>
+                            <button type="button" 
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                setIcon(i.ref)
+                              }}>
+                              <div className="flex p-2 size-10 rounded-lg items-center justify-center transition-all duration-150" 
+                                style={{backgroundColor: 
+                                  i.ref === icon ? color : `color-mix(in srgb, ${color} 10%, white)`
+                                }}> 
+                                
+                                <svg className="size-7" style={{color: i.ref === icon ? "white" :`color-mix(in srgb, ${color} 80%, white)`}}>
                                   <use href={i.ref}></use>
                                 </svg>
                               </div>
@@ -112,8 +137,14 @@ export default function CreateCategoryModal({isOpen, onOpenChange
                           ))}
                       </div>
 
-                    </div>
+                    
 
+                    </div>
+                  {/* Botonos accionadores */}
+                    <div className="flex w-full justify-center items-center gap-5">
+                          <button type="button" onClick={() => {onOpenChange(false)}} className="font-light text-text hover:bg-secondary/10 rounded-xl w-[50%] py-2 ring-1 ring-secondary-text/30 text-sm">Cancelar</button>
+                          <button type="submit" className="text-white text-sm rounded-xl w-[50%] py-2" style={{backgroundColor: color}}>Crear Categoría</button>
+                    </div>
                 </form>
               </section>
                 
